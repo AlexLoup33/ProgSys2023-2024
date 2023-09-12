a699ba38327580e79962f7f0f2f18f9a3bace74f
Lou-Poueyou Alexandre <a.loupoueyou@icloud.com>
2023-09-12 11:14:12 +0200

    [Update]: td1 archi and mytee.c

--- td1/mytee.c
+++ td1/mytee.c
@@ -7,11 +7,17 @@
 
 #include "error.h"
 
+//Write on the standard output
 int main(int argc, char **argv)
 {
   assert (argc == 2); // Nom du fichier de sortie attendu en ligne de commande
-
-  //....
-
+  char c;
+  int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  check(fd != -1, "open failed");
+  while(read(0, &c, 1)){
+    printf("%c", c);
+    write(fd, &c, 1);
+  }
+  close(fd);
   return EXIT_SUCCESS;
 }
